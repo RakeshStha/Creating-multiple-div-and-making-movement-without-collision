4ac22b684cd6c69820f341d40755ca70068c4eec
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2021-05-09 14:48:12 +0545

    Complete movement wothout collision

--- js/try.js
+++ js/try.js
@@ -1,63 +1,84 @@
+var xposition = 1;
+var yposition = 1;
 
-var i = 1;
+var div = new Array();
+var Xposition = new Array();
+var Yposition = new Array();
+
+window.onload = createDiv();
+
+// var counter = 0;
+// paragraph = document.getElementById("counter-display");
+// const cnt = document.createElement("p");
+// cnt.innerText("ants killed:" + counter);
+// paragraph.append(cnt);
 
 
-function createDiv(obj) {
+
+function createDiv() {
 
     
-                    for (j=0; j<obj; j++){
+                    for (i=0; i<5; i++){
     
-                        var div = document.createElement("div");
-                            div.setAttribute('id', 'div_id' + i );
-                            div.setAttribute('onclick', 'countClick()');
+                        var divs = document.createElement("div");
+                            divs.setAttribute('id', 'div_id' + i );
+                            divs.setAttribute('onclick', 'countClick()');
+                            divs.innerHTML = "Div" + i;
                     
-                            div.style = "position:absolute;height: 50px;width: 50px; ";
+                            divs.style = "position:absolute;text-align:center;padding-top: 15px; height: 50px;width: 50px; top:" + X + "px; left:" + Y + "px;";
 
-                            let randomPosition = ['20', '30','40','50', '60', '70', '80', '90', '100'];
-
-                            var top =  randomPosition[Math.floor(Math.random() * randomPosition.length)] ;
-                            var left =  randomPosition[Math.floor(Math.random() * randomPosition.length)];
-
-                            div.style.top = top + "px";
-                            div.style.left = left + "px";
+                            var X =  Math.floor((Math.random() * 420)+ 1) ;
+                            var Y =  Math.floor((Math.random() * 420)+ 1);
 
                             const randomColor = Math.floor(Math.random()*16777215).toString(16);
-                            div.style.background = "#" + randomColor;
-                    
-                            const content = document.querySelector(".div");
-                            content.appendChild(div);
-                    
-                            // obj.value = '';
-                    
-                            i = i + 1;
-
-                        }
-                        var y = parseInt(top);
-                            var x = parseInt(left);
-                       
+                            divs.style.background = "#" + randomColor;
                             
-
-                            setInterval(Move, 100);
-                            function Move(){
-                                y += 1;
-                                x += 1;
-                                
-                                div.style.top = x + "px";
-                                div.style.left = y + "px";
-                                
-                                console.log(x)
-                                console.log(y)
-
-                                   
-                                // div.style.top = X + "px";
-                                // div.style.left = Y + "px";
-                                // X += 1;
-                                // Y += 1; 
-                                // div.style.top = X + "px";
-                                // div.style.left = Y + "px";
-                            }
-                       
+                            const maindiv = document.getElementById("container");
+                            maindiv.append(divs);              
+                            Movement(0.2, 0.2, X, Y, divs, i)
+                            
+                        }    
                     }
-                        
 
-                    
+
+function countClick() {
+
+}
+
+function Movement(xposition, yposition, X, Y, divs, index){
+    setInterval(() => {
+        X += xposition;
+        divs.style.left = X + "px";
+
+        Y += yposition;
+        divs.style.top = Y + "px";
+        
+
+        if(X > 450 || X < 0){
+            xposition = xposition * -1;
+        }
+        if (Y > 450 || Y < 0) {
+            yposition = yposition * -1;
+        }
+        Xposition[index] = X;
+        Yposition[index] = Y;
+
+        for (var i = 0; i < 5; i++) {
+
+            if (!(i === index)) {
+                if ((Xposition[i] < X + 50) && (Xposition[i] + 50 > X) && (Yposition[i] < Y + 50) && (Yposition[i] + 50 > Y)) {
+                    xposition = xposition * -1;
+                    yposition = yposition * -1;
+                }
+            }
+        }
+    }, 1);
+
+
+}
+
+
+
+
+
+
